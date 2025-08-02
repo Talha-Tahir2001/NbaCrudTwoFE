5c046915ae819f401a10ac92444fd4da0b4d1bee
Talha Tahir <140109354+Talha-Tahir2001@users.noreply.github.com>
2025-08-02 16:39:36 +0500

    feat: add a player model

---
+++ src/app/Models/Player-Model.ts
@@ -0,0 +1,12 @@
+export type Player = {
+  id: number;
+  firstName: string;
+  lastName: string;
+  team: string;
+  age: number;
+  position: string;
+  height: string;
+  college: string;
+  country: string;
+  draftYear: Date;
+}
