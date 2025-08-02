f1d02e096e593898ab8c8b82d78924b94f0e99b9
Talha Tahir <140109354+Talha-Tahir2001@users.noreply.github.com>
2025-08-02 16:30:12 +0500

    refactor: app component
    - add a header
    - set render mode to client
    - add routes for home, player-list, add-player

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,30 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+//   {
+//     path: '',
+//     async loadComponent() {
+//       const m = await import('./pages/home/home');
+//       return m.Home;
+//     },
+//   },
+//   {
+//     path: 'player',
+//     async loadComponent() {
+//       const m = await import('./pages/player/player');
+//       return m.Player;
+//     },
+//   },
+    {
+        path: '',
+        loadComponent: () => import('./pages/home/home').then(m => m.Home),
+    },
+    {
+        path: 'players-list',
+        loadComponent: () => import('./pages/all-players/all-players').then(m => m.Player),
+    },
+    {
+        path: 'add-player',
+        loadComponent: () => import('./pages/add-player/add-player').then(m => m.AddPlayer),
+    }
+];
